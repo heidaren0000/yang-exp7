5c84e253a7a8eeb107914f723a29c46a7db8a503
yangkaiqi <heidaren0000@hotmail.com>
2023-05-14 22:06:46 +0800

    init base

---
+++ app/src/main/java/com/example/ykq_demo_07/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ykq_demo_07;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceStates) {
+        super.onCreate(savedInstanceStates);
+        setContentView(R.layout.activity_main);
+    }
+}
