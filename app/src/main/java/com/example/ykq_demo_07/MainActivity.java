422c1fe0aea7236b730b9b0f9d9bd95c960bde8e
yangkaiqi <heidaren0000@hotmail.com>
2023-05-15 21:24:02 +0800

    finished (barely

--- app/src/main/java/com/example/ykq_demo_07/MainActivity.java
+++ app/src/main/java/com/example/ykq_demo_07/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.ykq_demo_07;
 
 import static com.example.ykq_demo_07.StudentBaseHelper.getVersion;
 
+import android.annotation.SuppressLint;
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
@@ -44,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 mDatabase = new StudentOldBaseHelper(mContext).getWritableDatabase();
+                Toast.makeText(getApplicationContext(), getString(R.string.toast_db_created), Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -66,19 +69,37 @@ public class MainActivity extends AppCompatActivity {
                 mValues.put(StudentDbSchema.StudentTable.Cols.MAJOR, "Computer Science");
 
                 mDatabase.insert(StudentDbSchema.StudentTable.NAME, null, mValues);
+
+                Toast.makeText(getApplicationContext(), getString(R.string.toast_data_inserted) + mValues.toString(), Toast.LENGTH_SHORT).show();
+
             }
         });
 
         mBtnAlterData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mDatabase.update(StudentDbSchema.StudentTable.NAME, null, mValues);
+                String newName = "yangkaiqi2";
+                String oldName = "yangkaiqi";
+                ContentValues alterValues = new ContentValues();
+                alterValues.put(StudentDbSchema.StudentTable.Cols.ID, "1");
+                alterValues.put(StudentDbSchema.StudentTable.Cols.AGE, "22");
+                alterValues.put(StudentDbSchema.StudentTable.Cols.NAME, "yangkaiqi2");
+                alterValues.put(StudentDbSchema.StudentTable.Cols.MAJOR, "Computer Science");
+                mDatabase.update(StudentDbSchema.StudentTable.NAME, alterValues, StudentDbSchema.StudentTable.Cols.NAME + " = ?",
+                        new String[] { oldName });
+
+                Toast.makeText(getApplicationContext(), getString(R.string.toast_data_updated) + newName, Toast.LENGTH_SHORT).show();
             }
         });
 
         mBtnDeleteData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String name4delete = "yangkaiqi";
+                mDatabase.delete(StudentDbSchema.StudentTable.NAME, StudentDbSchema.StudentTable.Cols.NAME + " = ?",
+                        new String[] {name4delete});
+                Toast.makeText(getApplicationContext(), getString(R.string.btn_text_db_delete) + name4delete, Toast.LENGTH_SHORT).show();
+
 
             }
         });
@@ -86,7 +107,19 @@ public class MainActivity extends AppCompatActivity {
         mBtnQueryData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Cursor cursor = mDatabase.query(StudentDbSchema.StudentTable.NAME,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null);
+                cursor.moveToPosition(0);
+                @SuppressLint("Range") String name = cursor.getString(cursor.getColumnIndex(StudentDbSchema.StudentTable.Cols.NAME));
+                @SuppressLint("Range") String age = cursor.getString(cursor.getColumnIndex(StudentDbSchema.StudentTable.Cols.AGE));
+                @SuppressLint("Range") String major = cursor.getString(cursor.getColumnIndex(StudentDbSchema.StudentTable.Cols.MAJOR));
+                @SuppressLint("Range") String id = cursor.getString(cursor.getColumnIndex(StudentDbSchema.StudentTable.Cols.ID));
+                Toast.makeText(getApplicationContext(), getString(R.string.toast_data_query) + " " + name + " " + age + " " + major + " " + id , Toast.LENGTH_SHORT).show();
             }
         });
     }
